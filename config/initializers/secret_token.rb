c6ee7cf7a6b73cad41c331a273728b8af146d6e9
Riaz <riaz@riaz-VirtualBox.(none)>
2013-11-28 00:25:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Institute::Application.config.secret_key_base = '1b178163f089e8d76c1706bba0729876b0ad76ef4ce74bf743c10c566992c9b53a26206bb2b3739729dd27a687ebeb27124cf81221fd574c94eeb9fcc9795016'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Institute::Application.config.session_store :cookie_store, key: '_institute_session'
