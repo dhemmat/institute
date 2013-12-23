4ff6f8e2a8b3a8404161bf5545568c2381fd09fd
dhemmat <dhemmat@gmail.com>
2013-12-22 20:35:25 -0400

    added show view to devotionals and created address and name helpers

--- app/controllers/devotionals_controller.rb
+++ app/controllers/devotionals_controller.rb
@@ -2,4 +2,8 @@ class DevotionalsController < ApplicationController
 	def index
 		@devotionals = Devotional.all
 	end
+
+	def show
+		@devotional = Devotional.find(params[:id])
+	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,27 @@ module ApplicationHelper
 		end
 	end
 
+	def full_name_builder(person)
+		full_name=''
+		(full_name += person.first_name+' ') if (person.first_name)
+		(full_name += person.middle_name+' ') if (person.middle_name)
+		(full_name += person.last_name+' ') if (person.last_name)
+		(full_name += person.second_last_name+' ') if (person.second_last_name)
+		(full_name += '('+person.nickname+')') if (person.nickname)
+		full_name
+	end
+
+	def short_name_builder(person)
+		full_name=''
+		(full_name += person.first_name+' ') if (person.first_name)
+		(full_name += person.last_name+' ') if (person.last_name)
+		(full_name += '('+person.nickname+')') if (person.nickname)
+		full_name
+	end
+
+	def short_address_builder(address)
+		address_string = address.address + ', '
+		address_string += address.neighborhood.name + ', '
+		address_string += address.neighborhood.sector.city.name + '.'
+	end
 end
